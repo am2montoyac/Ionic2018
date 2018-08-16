f830da4d22a0517264a3619a8f893df73a7d9699
fmogollon13 <fmogollon@wi-mobile.com>
2018-08-15 21:33:58 -0500

    First Commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  constructor(public navCtrl: NavController, 
+    public alertCtrl:AlertController) { }
+  showAlert() {
+    const alert = this.alertCtrl.create({
+      title: 'New Friend!',
+      subTitle: 'Your friend, Obi wan Kenobi, just accepted your friend request!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+}
+
+export class SaladPage {
+  cucumber: boolean;
+
+  updateCucumber() {
+    console.log('Cucumbers new state:' + this.cucumber);
+  }
+}
